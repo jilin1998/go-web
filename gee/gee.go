1fbadfbe68a1a4f4d1ee30e4f75aaf567f0fc61f
jilin <jilin_cq@163.com>
2023-07-24 10:47:34 +0800

    feat:新增静态资源，url过滤，正则去除多个//

--- gee/gee.go
+++ gee/gee.go
@@ -3,6 +3,8 @@ package gee
 import (
 	"log"
 	"net/http"
+	"path"
+	"regexp"
 	"strings"
 )
 
@@ -46,6 +48,8 @@ func (group *RouterGroup) Group(prefix string) *RouterGroup {
 // 添加新路由
 func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
 	pattern := group.prefix + comp
+	reg, _ := regexp.Compile("/+")
+	pattern = string(reg.ReplaceAll([]byte(pattern), []byte("/")))
 	log.Printf("Route %4s - %s", method, pattern)
 	group.engine.router.AddRouter(method, pattern, handler)
 }
@@ -74,6 +78,10 @@ func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	//匹配中间件
 	var middlewares []HandlerFunc
+	//url处理，去掉重复的/
+	reg, _ := regexp.Compile("/+")
+	req.URL.Path = string(reg.ReplaceAll([]byte(req.URL.Path), []byte("/")))
+
 	for _, group := range engine.groups {
 		if strings.HasPrefix(req.URL.Path, group.prefix) {
 			middlewares = append(middlewares, group.middlewares...)
@@ -83,4 +91,25 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c.handlers = middlewares
 	engine.router.handle(c)
 
+}
+
+// 创建静态资源handler,文件交给http库来处理
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+		fileServer.ServeHTTP(c.Wraiter, c.Req)
+	}
+}
+
+// 静态资源处理方法注册
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPath := path.Join(relativePath, "/*filepath")
+	group.GET(urlPath, handler)
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"example/gee"
 	"log"
-	"net/http"
 	"time"
 )
 
@@ -20,19 +19,7 @@ func onlyForV2() gee.HandlerFunc {
 
 func main() {
 	r := gee.New()
-	r.Use(gee.Logger()) // global midlleware
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-	})
-
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2()) // v2 group middleware
-	{
-		v2.GET("/hello/:name", func(c *gee.Context) {
-			// expect /hello/geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-		})
-	}
+	r.Static("/assets", "D:/go_project/gee-web")
 
 	r.Run(":9999")
 }
